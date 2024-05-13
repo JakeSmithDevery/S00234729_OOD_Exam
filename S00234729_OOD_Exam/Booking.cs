be9d58162355867a88136fdd4c3304b3112128ce
Jake Devery <S00234729@atu.ie>
2024-05-13 13:14:23 +0100

    Q1(c) and 1(d) complete

---
+++ S00234729_OOD_Exam/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00234729_OOD_Exam
+{
+    //Class for Q1(c)
+    public class Booking
+    {
+        //vars for Q1(c)
+        public int BookingID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+    }
+}
---
+++ S00234729_OOD_Exam/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00234729_OOD_Exam
+{
+    //Class for Q1(d)
+    public class Customer
+    {
+        //Vars for Q1(d)
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        
+        public string ContactNumber { get; set; } 
+    }
+}
